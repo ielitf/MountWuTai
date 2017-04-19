f6d5016de6fc94dbe6f96a12e768f9ac47689dc4
e <893305119@qq.com>
2017-04-19 16:38:03 +0800

    1，添加“假数据”文件夹。用于分类放置假数据 2，修改CommonFragment,添加类型标签，根据不同的类型，加载不同的“假数据"

--- app/src/main/java/com/bupt/mountwutai/consts/CodeConstants.java
+++ app/src/main/java/com/bupt/mountwutai/consts/CodeConstants.java
@@ -7,4 +7,7 @@ package com.bupt.mountwutai.consts;
 public class CodeConstants {
     public static final String IS_LAZY_LOAD="isLazyLoad";
     public static final String TAB_INDEX="tabIndex";
+    //做一些标签
+    public static final String TYPE = "type";
+    public static final String TEPMLE_SUMMARY = "temple_summary";//寺庙一览
 }
