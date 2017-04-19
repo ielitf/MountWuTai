4ccabebddbca83cd9fa6a7c5aaf72853a85560db
Wyf <962283089@qq.com>
2017-04-19 15:45:13 +0800

    更新

--- app/src/main/java/com/bupt/mountwutai/ui/activity/guide/GuideFragment.java
+++ app/src/main/java/com/bupt/mountwutai/ui/activity/guide/GuideFragment.java
@@ -2,18 +2,14 @@ package com.bupt.mountwutai.ui.activity.guide;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.bupt.mountwutai.R;
-import com.bupt.mountwutai.adapter.PopAdapter;
 import com.bupt.mountwutai.base.BaseFragment;
 import com.bupt.mountwutai.consts.CallBack;
 import com.bupt.mountwutai.util.Utils;
-import com.bupt.mountwutai.widget.NoScrollListView;
 
 import java.util.ArrayList;
 
@@ -32,6 +28,8 @@ public class GuideFragment extends BaseFragment {
     ArrayList<String> list;
     //当前选中的列表项位置
     int clickPsition = -1;
+
+    TrafficGuideFragment trafficGuideFragment = null;
 
     @Override
     protected void onCreateView(Bundle savedInstanceState) {
@@ -61,6 +59,27 @@ public class GuideFragment extends BaseFragment {
                             @Override
                             public void itemClick(int position) {
                                 myButton.setText(list.get(position));
+                                switch (position){
+                                    case 0://行程规划
+
+                                        break;
+
+                                    case 1://交通指南
+
+                                        break;
+
+                                    case 2://酒店
+
+                                        break;
+
+                                    case 3://五台食谱
+
+                                        break;
+
+                                    case 4://朝台攻略
+
+                                        break;
+                                }
                                 if (clickPsition != position) {
                                     clickPsition = position;
                                 }
