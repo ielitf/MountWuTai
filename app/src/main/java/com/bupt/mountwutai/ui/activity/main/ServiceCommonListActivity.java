52ed1a16be2e7078238aacd451dcce04a9bd4bc6
e <893305119@qq.com>
2017-05-24 15:46:26 +0800

    111111111111111111

--- app/src/main/java/com/bupt/mountwutai/ui/activity/main/ServiceCommonListActivity.java
+++ app/src/main/java/com/bupt/mountwutai/ui/activity/main/ServiceCommonListActivity.java
@@ -24,6 +24,7 @@ public class ServiceCommonListActivity extends BaseActivity {
     private ListView businessTypeListView;
     private BusinessTypeListAdapter adapter;
     private int[] icons = {R.mipmap.buiness_tv1, R.mipmap.buiness_tv2, R.mipmap.buiness_tv3};
+    private List<BusinessTypeListBean> beanList;
 
     @Override
     protected void setLayout() {
@@ -34,24 +35,34 @@ public class ServiceCommonListActivity extends BaseActivity {
     protected void initView() {
         type = getIntent().getStringExtra(CodeConstants.TYPE);
         businessTypeListView = (ListView) findViewById(R.id.addr_info_listview);
-        List<BusinessTypeListBean> beanList = (List<BusinessTypeListBean>)
-                Utils.parseData(BroadcastingCenterDate.tv_service, new Gson(),
-                        new TypeToken<List<BusinessTypeListBean>>() {
-                        });
-        for (int i = 0; i < beanList.size(); i++) {
-            beanList.get(i).setIcon(icons[i]);
-        }
-        adapter = new BusinessTypeListAdapter(ServiceCommonListActivity.this, beanList);
-        businessTypeListView.setAdapter(adapter);
-
         switch (type){
             case CodeConstants.BUSINESS_NEW://新装业务
+                title = "新装业务";
+                beanList = (List<BusinessTypeListBean>)
+                        Utils.parseData(BroadcastingCenterDate.new_clothes_business, new Gson(),
+                                new TypeToken<List<BusinessTypeListBean>>() {
+                                });
                 break;
             case CodeConstants.BUSINESS_SALES://优惠活动
+                title = "优惠活动";
+                beanList = (List<BusinessTypeListBean>)
+                        Utils.parseData(BroadcastingCenterDate.preferential_activities, new Gson(),
+                                new TypeToken<List<BusinessTypeListBean>>() {
+                                });
                 break;
             case CodeConstants.BUSINESS_MAC://宽带业务
+                title = "宽带业务";
+                beanList = (List<BusinessTypeListBean>)
+                        Utils.parseData(BroadcastingCenterDate.broadband_business, new Gson(),
+                                new TypeToken<List<BusinessTypeListBean>>() {
+                                });
                 break;
         }
+//        for (int i = 0; i < beanList.size(); i++) {
+//            beanList.get(i).setIcon(icons[i]);
+//        }
+        adapter = new BusinessTypeListAdapter(ServiceCommonListActivity.this, beanList);
+        businessTypeListView.setAdapter(adapter);
     }
 
     @Override
